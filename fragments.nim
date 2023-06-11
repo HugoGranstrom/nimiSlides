35d7001e6d36239fe5f2f29895a5ce7f2c9f0c72
HugoGranstrom <HugoGranstrom@users.noreply.github.com>
2023-06-11 19:58:13 +0000

    deploy: a32d2b99ce1a2b9445abac5b5892a0c1b63b5909

---
+++ fragments.nim
@@ -0,0 +1,78 @@
+import nimib
+import nimiSlides
+
+nbInit(theme = revealTheme)
+setSlidesTheme(Moon)
+nb.useLatex
+
+slide:
+  nbText: md"""
+  # Fragments
+  This slideshow is a showcase of the fragments capabilities of nimiSlides. For the source code, look [here](https://github.com/HugoGranstrom/nimiSlides/blob/main/docs/fragments.nim).
+  """
+
+slide:
+  slide:
+    nbText: "### The basic animation is `fadeIn`:"
+    fragment:
+      nbText: "Fade in!"
+    fragment(fadeInThenOut):
+      nbText: "Fade in, then out!"
+    fragment(@[fadeIn], @[highlightBlue]):
+      nbText: "Fade in, then highlight text in blue!"
+    fragmentFadeIn(highlightBlue):
+      nbText: "Fade in, then highligt text in blue! (again but this time using `fragmentFadeIn`)"
+    fragment(@[fadeIn], @[highlightBlue, shrinks]):
+      nbText: "And now the same but a shrink at the same time as the highlighting!!"
+    fragmentFadeIn(@[highlightCurrentGreen, grows], @[shrinks]):
+      nbText: "Fade in, then highlight green and grows. Then shrink."
+
+  slide:
+    nbText: "### Nesting of fragments"
+    fragment:
+      nbText: "This is the first part"
+      fragment(fadeUp):
+        nbCode: echo "This code faded up"
+    fragment:
+      nbText: "Now the entire second part is shown"
+      nbCode: echo "This code was shown without any animations :("
+
+  slide:
+    nbText: "### Ending animations"
+    fragmentEnd(semiFadeOut): # When all code of the block has been run, everything in it will semiFadeOut. 
+      # fragment(semiFadeOut) would instead semiFadeOut everything before the code in the block has even been shown.
+      fragment:
+        nbText: "This is first part"
+        fragment(fadeUp):
+          nbCode: echo "This is faded up!"
+      fragment:
+        nbText: "This is second part!"
+        nbCode: echo "When all this has been shown, semiFadeOut!"
+
+slide:
+  let codeAnimations = [@[fadeIn], @[grows], @[shrinks, semiFadeOut]] # you can save animations for easy reuse
+  nbText: "### Let's make some animations to showcase code blocks one after another:"
+  fragmentThen(fadeIn, semiFadeOut):
+    nbCode:
+      let bla = "This will fadeIn and then semiFadeOut at the same time as the next block appears"
+  fragment(codeAnimations):
+    nbCode:
+      let s = """This code block will fadeIn,
+              grows and then shrink and semiFadeOut!"""
+  fragment(codeAnimations):
+    nbCode:
+      let t = "This code block will behave the same!"
+  fragment(codeAnimations):
+    nbCode:
+      proc hello(world: string) =
+        echo world
+      let r = "This too!"
+      hello(r)
+
+slide:
+  nbText: "Let's pass the green down the line!"
+  for i in 0 .. 5:
+    fragment(highlightCurrentGreen):
+      nbText: $i
+
+nbSave
