d0b7d5691354c1a326c9922fb4c74fa5e1bb9622
HugoGranstrom <5092565+HugoGranstrom@users.noreply.github.com>
2023-06-13 14:57:17 +0200

    create outline for the documentation structure

--- docsrc/tutorials/index.md
+++ docsrc/tutorials/index.md
@@ -1,2 +1,3 @@
 # Tutorials
 This section contains tutorials covering the features of nimiSlides.
+!<-- Add a table here with link to each tutorial and which templates are covered there -->
--- nbook.nim
+++ nbook.nim
@@ -4,5 +4,23 @@ var book = initBookWithToc:
   entry("Documentation", "index.nim", numbered = false)
   section("Tutorials", "tutorials/index.md"):
     entry("Getting Started", "getting_started.nim")
+    entry("Code Blocks", "code_block.nim")
+    entry("Text Formatting", "text_formatting.nim")
+    entry("Images & Media", "images_media.nim")
+    entry("Layout", "layout.nim")
+    section("Fragments (Animations)", "fragments/index.html"):
+      entry("Basics of Fragments", "basics.nim")
+      entry("Animated Lists", "lists.nim")
+      entry("List of Fragments", "list_fragments.nim")
+      entry("End Fragments", "end_fragments.nim")
+      entry("Advanced Fragments", "advanced_fragments.nim")
+    entry("Slide Options", "slide_options.nim")
+    entry("Backgrounds", "backgrounds.nim")
+    entry("Themes", "themes.nim")
+    entry("Speaker View", "speaker.nim")
+    entry("Local Reveal.js installation", "local_reveal.nim")
+    entry("Misc", "misc.nim")
+
+  entry("Gotchas", "gotchas.nim")
 
 nimibookCli(book)
