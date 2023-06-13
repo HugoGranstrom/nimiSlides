9091c6e37eb769550ce86fb4c5b14b9ba962d7f0
HugoGranstrom <5092565+HugoGranstrom@users.noreply.github.com>
2023-06-13 22:42:17 +0200

    code blocks tutorial

--- docsrc/tutorials/code_block.nim
+++ docsrc/tutorials/code_block.nim
@@ -1,5 +1,93 @@
-import nimib, nimibook
+import nimib, nimibook, nimiSlides
+import ../utils/embeddedReveal
 
 nbInit(theme = useNimibook)
-nbText: "## Code Blocks"
+
+initEmbeddedSlides()
+
+nbText: hlMd"""
+# Code Blocks
+The perhaps biggest reason why you are using nimiSlides is that you want to show off some Nim code.
+Showing a code block and optionally its printed output is done using the `nbCode` or `nbCodeInBlock` templates.
+The difference is that `nbCode` does not open a new scope, and as such variables defined in a `nbCode` block can be accessed in later blocks.
+`nbCodeInBlock` on the other hand opens a new block and the variables defined there can't be accessed outside it.
+This is important because `nbCodeInBlock` will allow us to reuse the same variable name multiple times.
+Here's an example where both are used:
+"""
+
+codeAndSlides:
+  slide:
+    nbCode:
+      let x = 1
+      let y = 2
+      echo x + y
+  slide:
+    nbCodeInBlock:
+      let a = 10
+      # note that x is from the previous nbCode block:
+      echo a + x
+
+nbText: hlMd"""
+We can see that both the code and the echo'd `3` is shown on the first slide.
+And on the second slide we can see that we access `x` because it was defined in a `nbCode` block.  
+
+But what if we don't want to show the output? Either because it is too long or just irrelevant.
+Then we can use `nbClearOutput()` directly after the call to `nbCode/nbCodeInBlock`:
+"""
+
+codeAndSlides:
+  slide:
+    nbCodeInBlock:
+      let x = 1
+      let y = 2
+      echo x + y
+    nbClearOutput()
+
+nbText: hlMd"""
+Poof! The output is gone! 
+
+## Animated Code Blocks
+One cool feature of Reveal.js is the animated code blocks.
+It allows you to create a code block like the ones we have created above, but highlight specific lines.
+This is done using the `animateCode` template by providing which lines to highlight and in which order.
+The lines can be specified either as a slice `x..y` or a single line number `x`.
+Note that the first line has line number 1 (and not 0).
+"""
+
+
+codeAndSlides:
+  slide:
+    animateCode(1, 2..3, 5, 3..4):
+      let x1 = 1
+      let x2 = 2
+      let x3 = 3
+      let x4 = 4
+      let x5 = 5
+
+nbText: hlMd"""
+As you can see, first line 1 is highlighted, then lines 2 through 3, then line 5 and lastly line 3 through 4. Neat!
+Note that there exists no `animateCodeInBlock` currently, but you can always wrap your `animateCode` inside a `block:` to get the same effect.
+The animated code block is also useful if you have a very long code snippet that you want to show as it will auto-scroll for you:
+"""
+
+codeAndSlides:
+  slide:
+    animateCode(1, 16, 5, 13, 1):
+      let y1 = 1
+      let y2 = 2
+      let y3 = 3
+      let y4 = 4
+      let y5 = 5
+      let y6 = 6
+      let y7 = 7
+      let y8 = 8
+      let y9 = 9
+      let y10 = 10
+      let y11 = 11
+      let y12 = 12
+      let y13 = 13
+      let y14 = 14
+      let y15 = 15
+      let y16 = 16
+
 nbSave
--- docsrc/utils/embeddedReveal.nim
+++ docsrc/utils/embeddedReveal.nim
@@ -6,13 +6,25 @@ template initEmbeddedSlides*(slidesTheme: SlidesTheme = Black) =
 <script src="https://cdnjs.cloudflare.com/ajax/libs/reveal.js/4.2.0/reveal.js" integrity="sha512-+Dy2HJZ3Z1DWerDhqFE7AH2HTfnbq8RC1pKOashfMwx1s01fjPUebWoHqrRedU1yFimkexmzJJRilKxjs7lz8g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/reveal.js/4.2.0/plugin/highlight/highlight.min.js" integrity="sha512-U3fPDUX5bMrn1wnYqjaK44MFA9E6MKS+zPAg9WPAGF5XhReBeDj3FGaA831CjueG+YJxYA3WaO/m33kMIoOs/A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/reveal.js/4.2.1/plugin/math/math.min.js" integrity="sha512-8eviRBLZHoiXLqXeMl5XurkjNEGizTI8DHbSUoGxkYFd4RslHpIYTEQmLYtWUemc5FfMYOkPDFUcQKefPLjF7A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/reveal.js/4.2.0/plugin/notes/notes.min.js" integrity="sha512-v2co+5nr0bgHekutTzF5jAB0UAjM95dpCF7VVw7WsFCjfxonbQo8Vwl487tNYl0iHWHHGV4o5xKBp5ifyhJkWg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/reveal.js/4.2.0/reveal.min.css" integrity="sha512-vFD6wFRj2whK8/X/dMgxJHinKfGlwMYtN+yRCxvxvmOgIiMIlgrFb5iOuCoqwCID+Qcq2/gY8DpmNHcAjfHWxw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/reveal.js/4.2.0/theme/$1.min.css" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/reveal.js/4.2.0/plugin/highlight/monokai.min.css" integrity="sha512-z8wQkuDRFwCBfoj7KOiu1MECaRVoXx6rZQWL21x0BsVVH7JkqCp1Otf39qve6CrCycOOL5o9vgfII5Smds23rg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 """ % [($slidesTheme).toLower()]
 
-  nb.partials["animateCode"] = "<pre style=\"width: 100%\"><code class=\"nim hljs\" data-noescape data-line-numbers=\"{{&highlightLines}}\">{{&codeHighlighted}}</code></pre>\n" & nb.partials["nbCodeOutput"]
+  nbRawHtml: """
+<style>
+table tbody tr:nth-child(2n) {
+  background: 0;
+}
+</style>
+"""
+
+  nb.partials["nbCodeSource"] = "<pre style=\"width: 100%\"><code class=\"nim hljs\" data-noescape data-line-numbers>{{&codeHighlighted}}</code></pre>"
+  nb.partials["nbCodeOutput"] = "{{#output}}<pre style=\"width: 100%;\"><samp class=\"hljs\">{{output}}</samp></pre>{{/output}}"
+
+  nb.partials["animateCode"] = "<pre style=\"width: 100%;\"><code class=\"nim hljs\" data-noescape data-line-numbers=\"{{&highlightLines}}\">{{&codeHighlighted}}</code></pre>\n" & nb.partials["nbCodeOutput"]
   nb.renderPlans["animateCode"] = nb.renderPlans["nbCode"]
 
   nb.partials["fragmentStart"] = """
@@ -29,6 +41,8 @@ template initEmbeddedSlides*(slidesTheme: SlidesTheme = Black) =
 
   nb.partials["bigText"] = """<h2 class="r-fit-text"> {{&outputToHtml}} </h2>"""
   nb.renderPlans["bigText"] = nb.renderPlans["nbText"]
+
+  nb.context["disableHighlightJs"] = true
 
 template embeddedSlides*(body: untyped) =
   currentFragment = 0
@@ -47,7 +61,12 @@ template embeddedSlides*(body: untyped) =
   <script>
     let deck_$1 = new Reveal(document.getElementById("$1"), {
       embedded: true,
-      keyboardCondition: 'focused'
+      keyboardCondition: 'focused',
+      plugins: [
+        RevealHighlight,
+        RevealNotes,
+        RevealMath.KaTeX
+      ]
     })
     deck_$1.initialize()
   </script>
