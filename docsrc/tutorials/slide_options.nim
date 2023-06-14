c2c4411b6022f8a3f09f2e82b5438bca1bd2eea8
HugoGranstrom <5092565+HugoGranstrom@users.noreply.github.com>
2023-06-14 22:33:12 +0200

    slide options docs

--- docsrc/tutorials/slide_options.nim
+++ docsrc/tutorials/slide_options.nim
@@ -1,5 +1,22 @@
-import nimib, nimibook
+import nimib, nimibook, nimiSlides
+import ../utils/embeddedReveal
 
 nbInit(theme = useNimibook)
-nbText: "## Slide Options"
+initEmbeddedSlides()
+
+nbText: hlMd"""
+# Slide Options
+The `slide` template can accept a `SlideOptions` object with some options.
+The options are related to [auto-animation](./auto_animate.html) and [backgrounds](./backgrounds.html),
+see their respective tutorials for more details.
+
+A `SlideOptions` is created using the proc `slideOptions`:
+"""
+
+codeAndSlides:
+  slide(slideOptions(colorBackground="#F1B434")):
+    nbText: "Yellow background!"
+
+
+
 nbSave
