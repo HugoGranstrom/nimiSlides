419e559225db664a4d3e6d7d7c3e40b7e5b79559
HugoGranstrom <5092565+HugoGranstrom@users.noreply.github.com>
2023-06-15 09:59:55 +0200

    backgrounds tutorials

--- docsrc/tutorials/slide_options.nim
+++ docsrc/tutorials/slide_options.nim
@@ -17,6 +17,16 @@ codeAndSlides:
   slide(slideOptions(colorBackground="#F1B434")):
     nbText: "Yellow background!"
 
+nbText: hlMd"""
+If you want to reuse the same `SlideOptions` for multiple slides,
+save it in a variable to reduce boilerplate:
+"""
 
+codeAndSlides:
+  let sOp = slideOptions(colorBackground="green")
+  slide(sOp):
+    nbText: "Green 1"
+  slide(sOp):
+    nbText: "Green 2"
 
 nbSave
