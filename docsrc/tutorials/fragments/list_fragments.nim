638c71e8faf04a30183f2c26658c1188e962874d
HugoGranstrom <5092565+HugoGranstrom@users.noreply.github.com>
2023-06-18 13:28:37 +0200

    list of fragments

--- docsrc/tutorials/fragments/list_fragments.nim
+++ docsrc/tutorials/fragments/list_fragments.nim
@@ -1,5 +1,44 @@
-import nimib, nimibook
+import nimib, nimibook, nimiSlides
+import ../../utils/embeddedReveal
+import std / strutils
 
 nbInit(theme = useNimibook)
-nbText: "## List of Fragments"
+initEmbeddedSlides()
+
+var names: array[FragmentAnimation, string]
+
+names[fadeIn] = "fadeIn" # the default
+names[fadeOut] = "fadeOut"
+names[fadeUp] = "fadeUp"
+names[fadeDown] = "fadeDown"
+names[fadeLeft] = "fadeLeft"
+names[fadeRight] = "fadeRight"
+names[fadeInThenOut] = "fadeInThenOut"
+names[fadeInThenSemiOut] = "fadeInThenSemiOut"
+names[grows] = "grows"
+names[semiFadeOut] = "semiFadeOut"
+names[shrinks] = "shrinks"
+names[strike] = "strike"
+names[highlightRed] = "highlightRed"
+names[highlightGreen] = "highlightGreen"
+names[highlightBlue] = "highlightBlue"
+names[highlightCurrentRed] = "highlightCurrentRed"
+names[highlightCurrentGreen] = "highlightCurrentGreen"
+names[highlightCurrentBlue] = "highlightCurrentBlue"
+
+for (frag, x) in names.pairs:
+  doAssert x.len > 0, "List of fragments not documented for fragment: $1" % [$frag]
+
+nbText: hlMd"""
+# List of Fragments
+This document lists and shows all the fragments (animations) available in nimiSlides.
+"""
+
+for frag in FragmentAnimation:
+  nbText: "## `$1`" % [names[frag]]
+  embeddedSlides:
+    slide:
+      fragment(frag):
+        nbText: names[frag]
+
 nbSave
