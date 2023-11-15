cbecb1e9cb13ce6cb816bea1997e561966ae0747
Hugo Granström <5092565+HugoGranstrom@users.noreply.github.com>
2023-11-15 06:41:35 +0100

    Update Reveal.js to 5.0.2 (#39)
    * update links
    
    * update pdf export docs
    
    * add useScrollView() to activate scroll view

--- docsrc/tutorials/pdf_export.nim
+++ docsrc/tutorials/pdf_export.nim
@@ -6,7 +6,7 @@ nbText: hlMd"""
 Reveal.js has support for exporting a slideshow to PDF using the print function in your browser.
 See [their documentation](https://revealjs.com/pdf-export/) for instructions.
 Here's a quick list of important notes:
-- Change the url from, for example `http://localhost:8000/slides.html` to `http://localhost:8000/slides.html?print-pdf`. This will show a printer-friendly version of your slides.
+- Change the url from, for example `http://localhost:8000/slides.html` to `http://localhost:8000/slides.html?view=print`. This will show a printer-friendly version of your slides.
 - It only works reliably on Chromium based browsers.
 """
 
