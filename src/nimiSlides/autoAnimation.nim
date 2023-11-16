a8d96c1af29ed3fc759a6d7c715d429f8e7e343e
HugoGranstrom <5092565+HugoGranstrom@users.noreply.github.com>
2023-11-16 19:29:38 +0100

    change name to showAt and introduce showUntil and showFrom

--- src/nimiSlides/autoAnimation.nim
+++ src/nimiSlides/autoAnimation.nim
@@ -6,14 +6,22 @@ template autoAnimateSlides*(nSlides: int, body: untyped) =
     slide(slideOptions(autoAnimate=true)):
       body
 
-template showOn*(showOnSlides: varargs[set[range[0..65535]], toSet], body: untyped) = # how to auto convert to set like in varargs?
+template showAt*(slideNrs: varargs[set[range[0..65535]], toSet], body: untyped) = # how to auto convert to set like in varargs?
   # use vararg and union all results!
   # This way you don't need to use the set syntax but can pass in `showOn(1, 2, 3)` instead.
   var totalSet: set[range[0..65535]]
-  for x in showOnSlides:
+  for x in slideNrs:
     totalSet.incl x
 
   if autoAnimateCounter in totalSet:
     body
   # The if-statement will cause some troubles for some code if it assumes it will be run in the same scope :/
   # To fix that we would need to require static inputs
+
+template showFrom*(slideNr: int, body: untyped) =
+  if autoAnimateCounter >= slideNr:
+    body
+
+template showUntil*(slideNr: int, body: untyped) =
+  if autoAnimateCounter <= slideNr:
+    body
